e81b641288c49ebfc23a11f9f0ce36316c8d2df0
João Otávio Peccia <cnttspk@gmail.com>
2025-05-05 10:56:17 -0300

    feat: create Garage page

---
+++ client/src/pages/Garage.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import { VehicleCard } from "../components/VehicleCard";
+
+export default function Garage() {
+    const [vehicles, setVehicles ] = useState([]);
+
+    useEffect(() => {
+        fetchVehicles().then(setVehicles);
+    }, []);
+
+    return (
+        <div className="p-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {vehicles.map((v: any) => (
+            <VehicleCard key={v.plate} vehicle={v} />
+        ))}
+        </div>
+    );
+}
