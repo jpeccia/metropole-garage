3cfe24a6513a73948152d485cf739265f0a84872
João Otávio Peccia <cnttspk@gmail.com>
2025-05-05 11:15:33 -0300

    feat: create initial structure to test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import { VehicleCard } from './components/VehicleCard';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [vehicle, setVehicle] = useState([]);
+
+  useEffect(() => {
+    fetchNui("garage:getVehicles", {}).then(setVehicle);
+  }, []);
 
   return (
-    <>
-<h1>Opa</h1>
-    </>
-  )
+    <div className="p-4 text-white bg-zinc-900 min-h-screen">
+      <h1 className="text-2xl font-bold mb-4">Minha Garagem</h1>
+      <VehicleCard vehicle={{ vehicle }} />
+    </div>
+  );
 }
 
-export default App
+const fetchNui = async (event: string, data: any) => {
+  const res = await fetch(`https://${GetParentResourceName()}/${event}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  return await res.json();
+};
+
+export default App;
+function GetParentResourceName() {
+  throw new Error('Function not implemented.');
+}
+
