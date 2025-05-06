f4ce95aa806fd7ecc01fab6a53d4d783d3597847
João Otávio Peccia <cnttspk@gmail.com>
2025-05-06 14:58:42 -0300

    feat (server): create vehicle controller

---
+++ server/src/controllers/vehicleController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+
+export const getAllVehiclesBySteamId = async (req: Request, res: Response) => {
+    const { steamId } = req.params;
+    try {
+        const vehicles = await vehicleService.getVehiclesBySteamId(steamId);
+        res.json(vehicles);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao buscar veiculos'});
+    }
+};
+
+export const getVehicleByPlate = async (req: Request, res: Response) => {
+    const { licensePlate } = req.params;
+    try {
+      const vehicle = await vehicleService.getVehicleByPlate(licensePlate);
+      if (!vehicle) return res.status(404).json({ error: 'Veículo não encontrado' });
+      res.json(vehicle);
+    } catch (error) {
+      res.status(500).json({ error: 'Erro ao buscar veículo' });
+    }
+};
