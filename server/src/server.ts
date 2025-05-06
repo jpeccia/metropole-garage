462a95b16d662d2fc594bd4838657c17a2a7cc14
João Otávio Peccia <cnttspk@gmail.com>
2025-05-06 15:08:51 -0300

    feat (server): add nui routes in server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import { config } from "dotenv";
 import express from 'express';
 import cors from 'cors';
 import vehicleRoutes from "./routes/vehicleRoutes";
+import nuiRoutes from "./routes/nuiRoutes";
 
 config();
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/vehicles', vehicleRoutes);
+app.use('/', nuiRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server rodando na porta ${PORT}`));
