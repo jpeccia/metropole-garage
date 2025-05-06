1e78140cbd131f6d4bb79992d31d357d99de5880
João Otávio Peccia <cnttspk@gmail.com>
2025-05-06 14:53:01 -0300

    feat (server): create server ts file

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import { config } from "dotenv";
+import express from 'express';
+import cors from 'cors';
+
+config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/vehicles', vehicleRoutes);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server rodando na porta ${PORT}`));
