66ae4c8371819a29917b21e8408557b540a03b55
João Otávio Peccia <cnttspk@gmail.com>
2025-05-08 19:31:52 -0300

    chore: removing unecessary files

--- dist/db.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.query = void 0;
-const query = (sql, values) => {
-    return global.exports.oxmysql.query(sql, values);
-};
-exports.query = query;
--- dist/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isAdmin = void 0;
-const isAdmin = (source) => {
-    return IsPlayerAceAllowed(String(source), 'metropole.admin');
-};
-exports.isAdmin = isAdmin;
